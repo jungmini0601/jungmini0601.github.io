7624d315467c7a9a0107659eb88805dd742ebdd8
김정민 <jungmini0601@gmail.com>
2023-09-18 15:03:20 +0900

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
-// 
-import 'prismjs/themes/prism-tomorrow.css'
